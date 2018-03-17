bf506bc581726fd0d379cb1b9a879a704e292402
Derek Ditch <derek@rocknsm.io>
2018-03-17 00:07:09 +0000

    Importing current logstash configs and elastic indexes. Needs suricata index still.

---
+++ configuration/elasticsearch/export-index-templates.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+ES_URL=http://127.0.0.1:9200
+
+for id in $(curl -s "${ES_URL}/_template" | jq -r 'keys[]' | grep -vE 'kibana|logstash'); do
+    echo "Exporting template named ${id} as ${id}.json" > /dev/stderr
+    curl -s "${ES_URL}/_template/${id}" | jq '.' > "${id}.json"
+done
+
