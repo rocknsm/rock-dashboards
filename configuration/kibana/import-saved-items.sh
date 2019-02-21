174be2937af08018decb4fd9fc9f5eacf5410243
Derek Ditch <derek@rocknsm.io>
2019-02-21 00:08:58 -0600

    Updates dashboards and fixes idx patterns - Enable Docket query links for Suricata and Bro - Fixes lingering viz with no index patterns - All the dashboards work now - Fixed bug in import/export scripts to better support round-trip.

--- configuration/kibana/import-saved-items.sh
+++ configuration/kibana/import-saved-items.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 _URL=$1
 KIBANA_URL=${_URL:=http://127.0.0.1:5601}
@@ -6,7 +6,7 @@ KIBANA_VERSION=$(curl -sI ${KIBANA_URL} | awk '/kbn-version/ { print $2 }')
 
 for item in index-pattern search visualization dashboard; do
     cd ${item} 2>/dev/null || continue
-    
+
     for id in $(cat index.json | jq -r '.[]'); do
         if curl -sI "${KIBANA_URL}/api/saved_objects/${item}/${id}" | grep -q '^HTTP.*404'; then
             # object doesn't exist, create it
@@ -28,9 +28,8 @@ for item in index-pattern search visualization dashboard; do
 done
 
 # Set default index
-defaultIndex=$(jq -r '.value' index-pattern/default.json)
+defaultIndex=$(jq -r '.userValue' index-pattern/default.json)
 
 echo "Setting defaultIndex to ${defaultIndex}" > /dev/stderr
 curl -s -XPOST -H"kbn-xsrf: true" -H"Content-Type: application/json" \
 	"${KIBANA_URL}/api/kibana/settings/defaultIndex" -d"{\"value\": \"${defaultIndex}\"}" >/dev/null
-
