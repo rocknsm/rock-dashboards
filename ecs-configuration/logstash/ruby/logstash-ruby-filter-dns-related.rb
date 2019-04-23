52076b4a5eca2d2e0c896af023a37278bc345e86
neu5ron <therealneu5ron@gmail.com>
2019-04-23 14:39:36 -0400

    ecs bro and very small portion of suricaita

--- ecs-configuration/logstash/ruby/logstash-ruby-filter-dns-related.rb
+++ ecs-configuration/logstash/ruby/logstash-ruby-filter-dns-related.rb
@@ -40,7 +40,7 @@ def filter(event)
   answers = event.get(@answers_field)
 
   related_ip = Set.new( event.get('[related][ip]'))
-  related_hostname = Set.new( event.get('[related][hostname]'))
+  related_hostname = Set.new( event.get('[related][domain]'))
 
   if (query_type == 'A' || query_type == 'AAAA') # A or AAAA
     related_hostname.add(query.downcase)
@@ -84,7 +84,7 @@ def filter(event)
     event.set( '[related][ip]', related_ip.to_a )
   end
   if related_hostname.size() > 0
-    event.set( '[related][hostname]', related_hostname.to_a )
+    event.set( '[related][domain]', related_hostname.to_a )
   end
 
   return [event]
@@ -95,7 +95,7 @@ test "when query type is A" do
   parameters {{"query_field" => "query", "query_type_field" => "qtype_name", "answers_field" => "answers" }}
   in_event {{"query" => "www.google.com", "qtype_name" => "A", "answers"=>["172.217.6.164", "216.58.218.100"]}}
   expect("related fields are set") {|events|
-    events.first.get("[related][hostname]") == ["www.google.com"]
+    events.first.get("[related][domain]") == ["www.google.com"]
     events.first.get("[related][ip]") == ["172.217.6.164", "216.58.218.100"]
   }
 end
@@ -104,7 +104,7 @@ test "when query type is AAAA" do
   parameters {{"query_field" => "query", "query_type_field" => "qtype_name", "answers_field" => "answers" }}
   in_event {{"query" => "www.google.com", "qtype_name" => "A", "answers"=>["2607:f8b0:4000:804::2004"]}}
   expect("related fields are set") {|events|
-    events.first.get("[related][hostname]") == ["www.google.com"]
+    events.first.get("[related][domain]") == ["www.google.com"]
     events.first.get("[related][ip]") == ["2607:f8b0:4000:804::2004"]
   }
 end
@@ -113,7 +113,7 @@ test "when query type is PTR" do
   parameters {{"query_field" => "query", "query_type_field" => "qtype_name", "answers_field" => "answers" }}
   in_event {{"query" => "100.218.58.216.in-addr.arpa", "qtype_name" => "PTR", "answers"=>["dfw25s07-in-f100.1e100.net", "dfw25s07-in-f4.1e100.net"]}}
   expect("related fields are set") {|events|
-    events.first.get("[related][hostname]") == ["100.218.58.216.in-addr.arpa", "dfw25s07-in-f100.1e100.net", "dfw25s07-in-f4.1e100.net"]
+    events.first.get("[related][domain]") == ["100.218.58.216.in-addr.arpa", "dfw25s07-in-f100.1e100.net", "dfw25s07-in-f4.1e100.net"]
     events.first.get("[related][ip]") == ["216.58.218.100"]
   }
 end
@@ -122,7 +122,7 @@ test "when query type is IPv6 PTR" do
   parameters {{"query_field" => "query", "query_type_field" => "qtype_name", "answers_field" => "answers" }}
   in_event {{"query" => "4.0.0.2.0.0.0.0.0.0.0.0.0.0.0.0.4.0.8.0.0.0.0.4.0.b.8.f.7.0.6.2.ip6.arpa", "qtype_name" => "PTR", "answers"=>["dfw25s17-in-x04.1e100.net"]}}
   expect("related fields are set") {|events|
-    events.first.get("[related][hostname]") == ["4.0.0.2.0.0.0.0.0.0.0.0.0.0.0.0.4.0.8.0.0.0.0.4.0.b.8.f.7.0.6.2.ip6.arpa", "dfw25s17-in-x04.1e100.net"]
+    events.first.get("[related][domain]") == ["4.0.0.2.0.0.0.0.0.0.0.0.0.0.0.0.4.0.8.0.0.0.0.4.0.b.8.f.7.0.6.2.ip6.arpa", "dfw25s17-in-x04.1e100.net"]
     events.first.get("[related][ip]") == ["2607:f8b0:4000:804::2004"]
   }
 end
