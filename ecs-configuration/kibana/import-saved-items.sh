43c3d86e7f524b2b7c4f387b882a129fde2bb9d5
neu5ron <therealneu5ron@gmail.com>
2019-08-26 16:24:21 -0400

    rewrite import and export for 7.3

--- ecs-configuration/kibana/import-saved-items.sh
+++ ecs-configuration/kibana/import-saved-items.sh
@@ -4,32 +4,52 @@ _URL=$1
 KIBANA_URL=${_URL:=http://127.0.0.1:5601}
 KIBANA_VERSION=$(curl -sI ${KIBANA_URL} | awk '/kbn-version/ { print $2 }')
 
-for item in index-pattern search visualization dashboard config; do
-    cd ${item} 2>/dev/null || continue
+updated=0
+created=0
+failed=0
 
-    for id in $(cat index.json | jq -r '.[]'); do
-        file="${id}.json"
-        if curl -sI "${KIBANA_URL}/api/saved_objects/${item}/${id}" | grep -q '^HTTP.*404'; then
-            # object doesn't exist, create it
-            echo "Creating ${item} with id ${id}" > /dev/stderr
-            curl -s -XPOST \
-                "${KIBANA_URL}/api/saved_objects/${item}/${id}" \
-                -H "kbn-xsrf: true" \
-                -H "Content-Type: application/json" \
-                -d "@-" \
-                <<< "{\"attributes\": $(cat "${file}")}" > /dev/null
-        else
-            # object already exists, apply update
-            echo "Overwriting ${item} named ${id}" > /dev/stderr
-            curl -s -XPUT \
-                "${KIBANA_URL}/api/saved_objects/${item}/${id}" \
-                -H "kbn-xsrf: true" \
-                -H "Content-Type: application/json" \
-                -d "@-" \
-                <<< "{\"attributes\": $(cat "${file}")}" > /dev/null
-        fi
-    done
-    cd ..
+for item in config index-pattern search visualization dashboard; do
+  cd ${item} 2>/dev/null || continue
+
+  for id in $(cat index.json | jq -r '.[]'); do
+    file="${id}.ndjson"
+    echo $file
+    if curl -sI "${KIBANA_URL}/api/saved_objects/${item}/${id}" | grep -q '^HTTP.*404'; then
+      # object doesn't exist, create it
+      echo "Creating ${item} with id ${id}" > /dev/stderr
+      response=$(
+        curl -s -XPOST \
+          "${KIBANA_URL}/api/saved_objects/_import" \
+          -H "kbn-xsrf: true" \
+          --form file=@"${file}"
+      )
+      result=$(echo "${response}" | jq -r '.success')
+      if [[ "${result}" == "true" ]]; then
+        created=$((created+1))
+      else
+        failed=$((failed+1))
+        echo -e "Failed creating ${file}: \n ${response}\n"
+      fi
+    else
+      # object already exists, apply update
+      echo "Overwriting ${item} named ${id}" > /dev/stderr
+      response=$(
+        curl -s -XPOST \
+          "${KIBANA_URL}/api/saved_objects/_import?overwrite=true" \
+          -H "kbn-xsrf: true" \
+          --form file=@"${file}"
+      )
+      result=$(echo "${response}" | jq -r '.success')
+      if [[ ${result} == "true" ]]; then
+        updated=$((updated+1))
+      else
+        failed=$((failed+1))
+        echo -e "Failed updating ${file}: \n ${response}\n"
+      fi
+
+    fi
+  done
+  cd ..
 done
 
 # Set default index
@@ -37,4 +57,8 @@ defaultIndex=$(jq -r '.userValue' index-pattern/default.json)
 
 echo "Setting defaultIndex to ${defaultIndex}" > /dev/stderr
 curl -s -XPOST -H"kbn-xsrf: true" -H"Content-Type: application/json" \
-    "${KIBANA_URL}/api/kibana/settings/defaultIndex" -d"{\"value\": \"${defaultIndex}\"}" >/dev/null
+  "${KIBANA_URL}/api/kibana/settings/defaultIndex" -d"{\"value\": \"${defaultIndex}\"}" >/dev/null
+
+echo "Created: ${created}"
+echo "Updated: ${updated}"
+echo "Failed: ${failed}"
